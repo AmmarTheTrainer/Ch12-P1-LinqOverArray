fa2ace307479b583bb9d759e06a97e6a533b994a
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-10-26 07:26:15 +0500

    Update Ch12-P1-LinqOverArray/Program.cs

--- Ch12-P1-LinqOverArray/Program.cs
+++ Ch12-P1-LinqOverArray/Program.cs
@@ -10,7 +10,29 @@ namespace Ch12_P1_LinqOverArray
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with LINQ to Objects *****\n");
 
+            QueryOverStrings();
+
+            Console.ReadLine();
+        }
+
+        private static void QueryOverStrings()
+        {
+            // Assume we have an array of strings.
+            string[] currentVideoGames = {"Dota", "NeedFor Speed2SE", "Fallout 3", "Daxter", "System Shock 2"};
+
+            // Build a query expression to find the items in the array
+            // that have an embedded space.
+            IEnumerable<string> subset = from g in currentVideoGames
+                                         where g.Contains(" ")
+                                         orderby g
+                                         select g;
+
+            foreach (string s in subset)
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
